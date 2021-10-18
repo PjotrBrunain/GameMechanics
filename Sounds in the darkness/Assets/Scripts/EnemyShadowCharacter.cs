4902c1703b314f959872421f2e8f8230fe80076d
PjotrBrunain <pjotr.brunain@gmail.com>
2021-10-18 12:31:11 +0200

    Made sound ripples and made enemys able to kill player

--- Sounds in the darkness/Assets/Scripts/EnemyShadowCharacter.cs
+++ Sounds in the darkness/Assets/Scripts/EnemyShadowCharacter.cs
@@ -6,17 +6,32 @@ public class EnemyShadowCharacter : BasicCharacter
 {
     private GameObject _playerTarget = null;
     [SerializeField] private float _attackRange = 2.0f;
+    private Health _playerHealth = null;
+    [SerializeField] private int _damage = 5;
+
+    private ParticleSystem _soundRippleSystem = null;
+
+    [SerializeField] private float _emitTime = 5.0f;
+    private float _accuTime = 0.0f;
 
     private void Start()
     {
         PlayerCharacter player = FindObjectOfType<PlayerCharacter>();
 
         if (player) _playerTarget = player.gameObject;
+
+        ParticleSystem rippleSystem = GetComponentInChildren<ParticleSystem>();
+
+        if (rippleSystem) _soundRippleSystem = rippleSystem;
+
+        _accuTime = Random.Range(0.0f, _emitTime);
     }
 
     private void Update()
     {
         HandleMovement();
+        HandleAttacking();
+        HandleRipple();
     }
 
     void HandleMovement()
@@ -24,6 +39,30 @@ public class EnemyShadowCharacter : BasicCharacter
         if (_movementBehavior == null) return;
 
         _movementBehavior.Target = _playerTarget;
+    }
+
+    void HandleAttacking()
+    {
+        if (_playerTarget == null) return;
+
+        if ((transform.position - _playerTarget.transform.position).sqrMagnitude < _attackRange * _attackRange)
+        {
+            _playerHealth = _playerTarget.GetComponent<Health>();
+            if (_playerHealth != null)
+            {
+                _playerHealth.Damage(_damage);
+            }
+        }
+    }
+
+    void HandleRipple()
+    {
+        _accuTime += Time.deltaTime;
+        if (_accuTime >= _emitTime)
+        {
+            _soundRippleSystem.Play();
+            _accuTime = 0.0f;
+        }
     }
 
     const string KILL_METHODNAME = "Kill";
